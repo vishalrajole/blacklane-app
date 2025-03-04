feece491c7d2c7e7dd9172f928b353e0341a9e6f
vishalrajole <vishalrajole1991@gmail.com>
2025-03-04 21:43:01 +0100

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: #f4f4f5;
+  color: #16161c;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Container>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          coun {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </Container>
+  );
+}
+
+export default App;
